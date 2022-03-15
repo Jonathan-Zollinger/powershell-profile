90049dd329ee16526ed1dd44683023a7c64c1e5e
jonathan-zollinger <jonathan.zollinger@microfocus.com>
2022-03-15 08:47:21 -0600

    adds write-octane-steps

--- General_Methods.ps1
+++ General_Methods.ps1
@@ -13,8 +13,52 @@ function Write-Comment(){
         #default to java comment
         $Comment_Character = "//"
     }
-    $SideBanner = "-" * ((100 - $Comment.Length)/2)
+    $SideBanner = "-" * ((25 - $Comment.Length)/2)
     Write-Output ("{0} {1} {2} {1}" -f $Comment_Character, $SideBanner, $Comment ) | Set-Clipboard
+}
+
+function Write-OctaneStep(){
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [String] $Text,
+        [Parameter(Mandatory = $false)]
+        [String] $Subtext,
+        [Parameter(Mandatory = $false)]
+        [switch] $Bold,
+        [Parameter(Mandatory = $false)]
+        [switch] $Italic,
+        [Parameter(Mandatory = $false)]
+        [switch] $Underline,
+        [Parameter(Mandatory = $false, ParameterSetName= "Color")]
+        [switch] $Green,
+        [Parameter(Mandatory = $false, ParameterSetName= "Color")]
+        [switch] $Blue,
+        [Parameter(Mandatory = $false, ParameterSetName= "Color")]
+        [switch] $Red
+    )
+    if ($Subtext.IsPresent) {
+        Write-Output ("**{0}**{1}" -f $Text, $Subtext) | Set-Clipboard 
+        return
+    }
+    if ($Bold.IsPresent) {
+        $Text = ("**{0}**" -f $Text)
+    }
+    if ($Italic.IsPresent) {
+        $Text = ("*{0}*" -f $Text)
+    }
+    if ($Underline.IsPresent) {
+        $Text = ("__{0}__" -f $Text)
+    }
+    if ($Green.IsPresent -or $Blue.IsPresent -or $Red.IsPresent) {
+        switch ($PSCmdlet.ParameterSetName) {
+            $Green  {$Text = ("`{{0}`}green" -f $Text)}
+            $Blue   {$Text = ("`{{0}`}blue" -f $Text)}
+            $Red    {$Text = ("`{{0}`}red" -f $Text)}
+        }
+    }
+    Write-Output $Text | Set-Clipboard
+    return $Text
 }
 function Write-Header{
     param([string[]] $Header)
